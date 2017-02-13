875585c6f1232ad85074f7ea7d4d73d4f52004e3
CJ DiMaggio <charlesdimaggio@localprojects.net>
2017-02-13 02:40:16 -0500

    Dependencies

--- addon_config.mk
+++ addon_config.mk
@@ -1,27 +1,46 @@
-ofxOpenCv
-ofxOsc
-ofxXmlSettings
-ofxNetwork
-../../ExternalAddons/ofxFontStash
-../../ExternalAddons/ofxTuio
-../../ExternalAddons/ofxJson
-../../ExternalAddons/ofxSuperLog
-../../ExternalAddons/ofxAutoTexture
-../../ExternalAddons/ofxApp
-../../ExternalAddons/ofxMTJsonParser
-../../ExternalAddons/ofxSimpleHttp
-../../ExternalAddons/ofxTagSystem
-../../ExternalAddons/ofxStateMachine
-../../ExternalAddons/ofxAssets
-../../ExternalAddons/ofxThreadSafeLog
-../../ExternalAddons/ofxProgressiveTextureLoad
-../../ExternalAddons/ofxTexturedObject
-../../ExternalAddons/ofxMullion
-../../ExternalAddons/ofxJsonSettings
-../../ExternalAddons/ofxTimeMeasurements
-../../ExternalAddons/ofxRemoteUI
-../../ExternalAddons/ofxHistoryPlot
-../../ExternalAddons/ofxAnimatable
-../../ExternalAddons/ofxScreenSetup
-../../ExternalAddons/ofxSensu
-../../ExternalAddons/ofxGoogleAnalytics
+meta:
+	ADDON_NAME = ofxRemoteUI
+	ADDON_DESCRIPTION = Tweak params from the network over OSC
+	ADDON_AUTHOR = Oriol Ferrer Mesià
+	ADDON_TAGS = "osc" "paramter" "tweak"
+	ADDON_URL = https://github.com/armadillu/ofxRemoteUI
+
+common:
+
+	# dependencies with other addons, a list of them separated by spaces
+	# or use += in several lines
+	  ADDON_DEPENDENCIES = ofxOSC ofxXmlSettings ofxFontStash ofxTuio ofxJson ofxSuperLog ofxAutoTexture ofxMtJsonParser ofxSimpleHttp ofxTagSystem ofxStateMachine ofxAssets ofxThreadSafeLog ofxProgressiveTextureLoad ofxTexturedObject ofxOpenCV ofxMullion ofxSensu ofxNetwork ofxGoogleAnalytics ofxJsonSettings ofxTimeMeasurements ofxRemoteUI ofxHistoryPlot ofxScreenSetup ofxPoco
+
+	# include search paths, this will be usually parsed from the file system
+	# but if the addon or addon libraries need special search paths they can be
+	# specified here separated by spaces or one per line using +=
+	# ADDON_INCLUDES =
+
+	# any special flag that should be passed to the compiler when using this
+	# addon
+	# ADDON_CFLAGS =
+
+	# any special flag that should be passed to the linker when using this
+	# addon, also used for system libraries with -lname
+	# ADDON_LDFLAGS =
+
+	# linux only, any library that should be included in the project using
+	# pkg-config
+	# ADDON_PKG_CONFIG_LIBRARIES =
+
+	# osx/iOS only, any framework that should be included in the project
+	# ADDON_FRAMEWORKS =
+
+	# source files, these will be usually parsed from the file system looking
+	# in the src folders in libs and the root of the addon. if your addon needs
+	# to include files in different places or a different set of files per platform
+	# they can be specified here
+	# ADDON_SOURCES =
+
+	# some addons need resources to be copied to the bin/data folder of the project
+	# specify here any files that need to be copied, you can use wildcards like * and ?
+	# ADDON_DATA =
+
+	# when parsing the file system looking for libraries exclude this for all or
+	# a specific platform
+	# ADDON_LIBS_EXCLUDE =
