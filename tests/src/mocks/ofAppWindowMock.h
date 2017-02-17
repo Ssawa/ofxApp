97ff8d6b07980df914a751161084d853928cc0ef
CJ DiMaggio <charlesdimaggio@localprojects.net>
2017-02-16 19:11:34 -0500

    Tweak some stuff

---
+++ tests/src/mocks/ofAppWindowMock.h
@@ -0,0 +1,13 @@
+//
+//  ofAppWindowMock.h
+//  TestApplication
+//
+//  Created by CJ DiMaggio on 2/16/17.
+//
+//
+
+#ifndef ofAppWindowMock_h
+#define ofAppWindowMock_h
+
+
+#endif /* ofAppWindowMock_h */
